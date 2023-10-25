07d75b7efb295e764dd66035bbd59992ceec8f11
Yusra Noor <126388167+missyussi@users.noreply.github.com>
2023-10-25 22:37:13 +0500

    hi

---
+++ src/components/navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+<ul>
+    <li><a>home</a></li>
+    <li><a>home</a></li>
+    <li><a>home</a></li>
+    <li><a>home</a></li>
+    <li><a>home</a></li>
+</ul>
+
+
+
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
