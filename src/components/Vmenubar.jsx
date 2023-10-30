c5904098f29f42d401f8dde2c6a93d42543c7a23
Yusra Noor <126388167+missyussi@users.noreply.github.com>
2023-10-30 21:18:28 +0500

    new commit

--- src/components/Vmenubar.jsx
+++ src/components/Vmenubar.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 function VmenuBar() {
   return (
+    <>
     <div>
-
+      <div className=' top-0 bottom-0 h-screen w-48 z-20 bg-gray-100 rounded-r-lg py-1 '>
+        <ul className='flex flex-col text-black px-20 p-2 mt-4 h-full'>
+          <li className='mt-6'>helllo</li>
+          <li className='mt-6'>helllo</li>
+          <li className='mt-6'>helllo</li>
+          <li className='mt-6'>helllo</li>
+          <li className='mt-6'>helllo</li>
+          <li className='mt-6'>helllo</li>
+        </ul>
+        </div>
     </div>
+    </>
   )
 }
 
