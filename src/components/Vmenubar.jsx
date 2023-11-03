2fb661b157e4a4f265a5dfd9d11a722fe358fb10
Yusra Noor <126388167+missyussi@users.noreply.github.com>
2023-11-03 22:49:18 +0500

    all set

--- src/components/Vmenubar.jsx
+++ src/components/Vmenubar.jsx
@@ -6,7 +6,7 @@ function VmenuBar({handleOpen,open,handleClose}) {
 
     <div style={open ? {display:"block"} : {display:"none"}}
      className='h-screen z-20  ml-6 flex flex-col fixed top-0 bottom-0 py-1'>
-      <nav className=' bg-white rounded-r-3xl h-full shadow-lg'>
+      <nav className=' bg-white rounded-r-3xl h-full shadow-2xl'>
 
         <ul className=' px-12  p-2  '>
          <div className=' flex gap-2'>
@@ -17,12 +17,12 @@ function VmenuBar({handleOpen,open,handleClose}) {
         </div>
         </ul>
 <ul className='pl-8 mt-12 text-sm'>
-          <li className='mt-5 hover:bg-orange-500 hover:text-white rounded-md mx-2 p-1'>Dashboard</li>
-          <li className='mt-5 hover:bg-orange-500 hover:text-white rounded-md mx-2 p-1'>Compaigns</li>
-          <li className='mt-5 hover:bg-orange-500 hover:text-white rounded-md mx-2  p-1'>Contact</li>
-          <li className='mt-5 hover:bg-orange-500 hover:text-white rounded-md mx-2  p-1'>Categories</li>
-          <li className='mt-5 hover:bg-orange-500 hover:text-white rounded-md mx-2 p-1'>Reporting</li>
-          <li className='mt-5 hover:bg-orange-500 hover:text-white rounded-md mx-2 p-1'>Email Template</li>
+          <li className='mt-5 hover:bg-orange-500 hover:text-white rounded-md mx-2 p-2'>Dashboard</li>
+          <li className='mt-5 hover:bg-orange-500 hover:text-white rounded-md mx-2 p-2'>Compaigns</li>
+          <li className='mt-5 hover:bg-orange-500 hover:text-white rounded-md mx-2  p-2'>Contact</li>
+          <li className='mt-5 hover:bg-orange-500 hover:text-white rounded-md mx-2  p-2'>Categories</li>
+          <li className='mt-5 hover:bg-orange-500 hover:text-white rounded-md mx-2 p-2'>Reporting</li>
+          <li className='mt-5 hover:bg-orange-500 hover:text-white rounded-md mx-2 p-2'>Email Template</li>
         </ul>
         </nav>
     </div>
