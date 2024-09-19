2218c3e84005b2dd2dcced0911b1f73feb7374d6
Yusra Noor <126388167+missyussi@users.noreply.github.com>
2024-09-19 14:33:35 +0500

    Update new.jsx

--- src/components/new.jsx
+++ src/components/new.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 function New(){
-  return (
-    <>
-
-    </>
-  )
-}
-
-export default New
+    return (
+      <>
+  
+      </>
+    )
+  }
+  
+  export default New
